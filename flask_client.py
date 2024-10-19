d6935462ce01b476a3149acbafa4832be1039817
0nhc <thefoxfoxfox@outlook.com>
2024-10-19 17:49:56 -0500

    flask server done

--- flask_client.py
+++ flask_client.py
@@ -1,14 +1,64 @@
 import requests
+import numpy as np
 
-# Base URL of the Flask server
-BASE_URL = "http://127.0.0.1:5000"  # Adjust the URL to your Flask server address
+from PIL import Image
 
-# 3. Sending a POST request to /post_example with JSON data
-def send_post_example(data):
-    response = requests.post(f"{BASE_URL}/get_gsnet_grasp", json=data)
-    print("POST /get_gsnet_grasp response:", response.json())
 
-# Example of how the client interacts with the server
-if __name__ == "__main__":
-    # Call the functions to interact with the Flask server
-    send_post_example({"name": "Han", "age": 22})  # Sends a POST request with JSON data
+class GSNetFlaskClient:
+    def __init__(self, base_url="http://127.0.0.1:5000"):
+        self._base_url = base_url
+
+        depth_path, intrinsics = self.DEMO_return_depth_data()
+        pcd = self.DEMO_get_pcd(depth_path, intrinsics)
+        grasping_pose = self.request_grasping_pose(pcd)
+        print(grasping_pose)
+
+
+    def request_grasping_pose(self, data):
+        response = requests.post(f"{self._base_url}/get_gsnet_grasp", json=data)
+        return response.json()
+    
+
+    def DEMO_return_depth_data(self):
+        depth_path = "example_data/depth.png"
+        intrinsics = np.array([[383.5498046875, 0.0, 320.0272521972656], 
+                               [0.0, 383.5498046875, 235.78729248046875], 
+                               [0.0, 0.0, 1.0]])
+        return depth_path, intrinsics
+
+
+    def DEMO_get_pcd(self, depth_path, intrinsics, depth_scale=1000):
+        # Load depth image
+        depth = np.asarray(Image.open(depth_path), dtype=np.uint16)
+        depth = depth[:,:,0]
+
+        # Load Intrinsics
+        height = depth.shape[0]
+        width = depth.shape[1]
+        center_x = intrinsics[0, 2]
+        center_y = intrinsics[1, 2]
+        focal_x = intrinsics[0, 0]
+        focal_y = intrinsics[1, 1]
+
+        # Convert depth image to point cloud
+        u_indices , v_indices = np.meshgrid(np.arange(width), np.arange(height))
+        x_factors = (u_indices - center_x) / focal_x
+        y_factors = (v_indices - center_y) / focal_y
+
+        z_mat = depth
+        x_mat = x_factors * z_mat
+        y_mat = y_factors * z_mat
+
+        points = []
+        for h in range(height):
+            for w in range(width):
+                x = x_mat[h, w]
+                y = y_mat[h, w]
+                z = z_mat[h, w] / depth_scale
+                points.append([x,y,z])
+        
+        return points
+                
+        
+    
+client = GSNetFlaskClient()
--- flask_server.py
+++ flask_server.py
@@ -1,13 +1,15 @@
 import sys
 import yaml
 import os
-
+import torch
+import numpy as np
 
 from flask import Flask, request, jsonify
 from models.graspnet import GraspNet, pred_decode
 from dataset.graspnet_dataset import minkowski_collate_fn
 from utils.collision_detector import ModelFreeCollisionDetector
 from utils.data_utils import CameraInfo, create_point_cloud_from_depth_image, get_workspace_mask
+from graspnetAPI.graspnet_eval import GraspGroup
 
 
 class GSNetFlaskServer:
@@ -19,64 +21,106 @@ class GSNetFlaskServer:
         self.app = Flask(__name__)
         self.setup_routes()
 
-        # GSNet related
+        # GSNet relateds
+        self._load_checkpoint()
 
-    
+        # Storage for point cloud and grasp
+        self.best_gg = None
+        self.pcd = None
+
     
     def setup_routes(self):
         @self.app.route('/get_gsnet_grasp', methods=['POST'])
         def get_gsnet_grasp():
+            # Get data from request, data should be a list of points
             data = request.json
-            return jsonify(data)
+
+            # Run GSNet inference
+            gsnet_input, pcd = self._as_gsnet_input(data)
+            # Get the best grasping pose
+            gg = self._gsnet_inference(gsnet_input)
+            best_grasp = self._get_best_grasping_pose(gg)
+
+            return jsonify(best_grasp.tolist())
     
 
     def run(self):
         self.app.run(debug=True)
 
 
-    # def _gsnet_inference(self, data_input):
-    #     batch_data = minkowski_collate_fn([data_input])
-    #     net = GraspNet(seed_feat_dim=self.cfg["seed_feat_dim"], is_training=False)
-    #     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    #     net.to(device)
-    #     # Load checkpoint
-    #     checkpoint = torch.load(self.cfg["checkpoint_path"])
-    #     net.load_state_dict(checkpoint['model_state_dict'])
-    #     start_epoch = checkpoint['epoch']
-    #     print("-> loaded checkpoint %s (epoch: %d)" % (self.cfg["checkpoint_path"], start_epoch))
+    def _get_best_grasping_pose(self, gg):
+        # Select the best grasping pose
+        gg = gg.nms()
+        gg = gg.sort_by_score()
+        self.best_gg = gg[0]
+        p = self.best_gg.translation
+        r = self.best_gg.rotation_matrix
+        t = np.asarray([[r[0][0], r[0][1], r[0][2], p[0]],
+                        [r[1][0], r[1][1], r[1][2], p[1]],
+                        [r[2][0], r[2][1], r[2][2], p[2]],
+                        [0,0,0,1]])
+        return t
 
-    #     net.eval()
 
-    #     for key in batch_data:
-    #         if 'list' in key:
-    #             for i in range(len(batch_data[key])):
-    #                 for j in range(len(batch_data[key][i])):
-    #                     batch_data[key][i][j] = batch_data[key][i][j].to(device)
-    #         else:
-    #             batch_data[key] = batch_data[key].to(device)
-    #     # Forward pass
-    #     with torch.no_grad():
-    #         end_points = net(batch_data)
-    #         grasp_preds = pred_decode(end_points)
+    def _as_gsnet_input(self, pcd):
+        cloud = np.asarray(pcd)
+        ret_dict = {'point_clouds': cloud.astype(np.float32),
+                    'coors': cloud.astype(np.float32) / self.cfg["voxel_size"],
+                    'feats': np.ones_like(cloud).astype(np.float32),
+                    }
+        return ret_dict, cloud
+    
 
-    #     preds = grasp_preds[0].detach().cpu().numpy()
+    def _load_checkpoint(self):
+        # Load checkpoint
+        self.net = GraspNet(seed_feat_dim=self.cfg["seed_feat_dim"], is_training=False)
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        self.net.to(self.device)
+        checkpoint = torch.load(self.cfg["checkpoint_path"])
+        self.net.load_state_dict(checkpoint['model_state_dict'])
+        start_epoch = checkpoint['epoch']
+        print("-> loaded checkpoint %s (epoch: %d)" % (self.cfg["checkpoint_path"], start_epoch))
 
-    #     gg = GraspGroup(preds)
-    #     # collision detection
-    #     if self.cfg["collision_thresh"] > 0:
-    #         cloud = data_input['point_clouds']
-    #         mfcdetector = ModelFreeCollisionDetector(cloud, voxel_size=self.cfg["voxel_size_cd"])
-    #         collision_mask = mfcdetector.detect(gg, approach_dist=0.05, collision_thresh=self.cfg["collision_thresh"])
-    #         gg = gg[~collision_mask]
+        # Set to eval mode
+        self.net.eval()
+        
 
-    #     return gg
+    def _gsnet_inference(self, data_input):
+        # Load data
+        batch_data = minkowski_collate_fn([data_input])
+
+        # Move data to device
+        for key in batch_data:
+            if 'list' in key:
+                for i in range(len(batch_data[key])):
+                    for j in range(len(batch_data[key][i])):
+                        batch_data[key][i][j] = batch_data[key][i][j].to(self.device)
+            else:
+                batch_data[key] = batch_data[key].to(self.device)
+        # Forward pass
+        with torch.no_grad():
+            end_points = self.net(batch_data)
+            grasp_preds = pred_decode(end_points)
+        
+        # Get predictions
+        preds = grasp_preds[0].detach().cpu().numpy()
+        gg = GraspGroup(preds)
+
+        # collision detection
+        if self.cfg["collision_thresh"] > 0:
+            cloud = data_input['point_clouds']
+            mfcdetector = ModelFreeCollisionDetector(cloud, voxel_size=self.cfg["voxel_size_cd"])
+            collision_mask = mfcdetector.detect(gg, approach_dist=0.05, collision_thresh=self.cfg["collision_thresh"])
+            gg = gg[~collision_mask]
+
+        return gg
     
 
     def _read_params(self):
-        cfg_path = sys.path[0]+'/config/gsnet.yaml'
+        cfg_path = sys.path[0]+'/config/gsnet_flask_server.yaml'
         with open(cfg_path, 'r') as config:
             self.cfg = yaml.safe_load(config)
-
+    
 
 gsnetflaskserver = GSNetFlaskServer()
 gsnetflaskserver.run()
